61cdc3a9f97c187d2a1f47048456ad0ee1b9818c
Dogugun Ozkaya <dogugun.ozkaya@inomera.com>
2012-05-19 12:22:04 +0300

    launcher icon changed

--- gen/com/inomera/mb/R.java
+++ gen/com/inomera/mb/R.java
@@ -13,7 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int delete=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int motorboat=0x7f020002;
+        public static final int mb_launcher=0x7f020002;
+        public static final int motorboat=0x7f020003;
     }
     public static final class id {
         public static final int MainFrame=0x7f050000;
