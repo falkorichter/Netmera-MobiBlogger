c8b545e14cee814ba440c0915acb80a5701e41a5
Falko Richter <falko.richter@sinnerschrader-mobile.de>
2012-11-28 21:06:33 +0100

    removed specific keys

--- src/com/inomera/mb/constans/GeneralConstants.java
+++ src/com/inomera/mb/constans/GeneralConstants.java
@@ -2,9 +2,9 @@ package com.inomera.mb.constans;
 
 public class GeneralConstants {
 	
-	public static final String NETMERA_API_KEY = "WVhCd1ZYSnNQV2gwZEhBbE0wRWxNa1lsTWtZMU1EUTNPRE0xTUM1dVpYUnRaWEpoTG1OdmJTVXpRVGd3SlRKR2JXOWlhVzFsY21FbE1rWm5ZV1JuWlhRbE1rWm9iMjFsTG5odGJDWnViVk5wZEdWVmNtdzlhSFIwY0NVelFTVXlSaVV5UmpVd05EYzRNelV3TG01bGRHMWxjbUV1WTI5dEpUTkJPREFtYlc5a2RXeGxTV1E5T0RrMU9TWmhjSEJKWkQwMU1EUTNPRE0xTUNadWJWUmxiWEJzWVhSbFBXMXZZbWwwWlcxd2JHRjBaU1p2ZDI1bGNrbGtQV05oWjNKcFkyVjBhVzQ0T1Naa2IyMWhhVzQ5Ym1WMGJXVnlZUzVqYjIwbWJtMVRhWFJsUFRVd05EYzRNelV3Sm05M2JtVnlVbTlzWlZSNWNHVTlNU1oyYVdWM1pYSlNiMnhsVkhsd1pUMHhKblpwWlhkbGNrbGtQV05oWjNKcFkyVjBhVzQ0T1NZ";
+	public static final String NETMERA_API_KEY = "<insert your netmera key here>";
 	
-	public static final String GOOGLE_API_KEY = "";
+	public static final String GOOGLE_API_KEY = "<insert the id of your appengine project here>";
 
 	public static final String TWITTER_CONSUMER_KEY = "";
 	public static final String TWITTER_CONSUMER_SECRET = "";
